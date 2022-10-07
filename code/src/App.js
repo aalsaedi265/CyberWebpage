b38b3e6e6f8998cb2d8cd747ec3829438715f400
aalsaedi265 <aalsaedi265@gmail.com>
2022-10-07 09:22:53 -0500

    pick and title

--- code/src/App.js
+++ code/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
+
 import './App.css';
 
 function App() {
+
+  useEffect(()=> {document.title = "Night City OverLords"},[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <div className='menu'>
+        <div className='logo'>
+          Cycberpunk
+          <div className='sublogo'>
+            2077
+          </div>
+        </div>
+        <ul>
+          <li>Nigh City News</li>
+          <li>Your Page</li>
+          <li>Highest Bounty</li>
+          <li>Saved</li>
+          <li>Developers</li>
+        </ul>
+      </div>
     </div>
   );
 }
