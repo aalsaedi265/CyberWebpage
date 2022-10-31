dbfeeaced44d9239f436624f97c5f87daf51a257
Ahmed Alsaedi <aalsaedi265@gmail.com>
2022-10-31 17:22:36 -0500

    being on profile

--- code/src/modules/news_feed/NightCityNews.js
+++ code/src/modules/news_feed/NightCityNews.js
@@ -1,25 +1,25 @@
 import React,{useState, useEffect} from "react";
-import axios from 'axios'
+// import axios from 'axios'
 
 function nighCityNews(){
 
-    const axios = require("axios");
+    // const axios = require("axios");
 
-    const options = {
-      method: 'GET',
-      url: 'https://bloomberg-market-and-financial-news.p.rapidapi.com/market/auto-complete',
-      params: {query: '<REQUIRED>'},
-      headers: {
-        'X-RapidAPI-Key': 'b27ef0319dmshd9f173a6bfc4399p1e5a8djsn089ae46aa96f',
-        'X-RapidAPI-Host': 'bloomberg-market-and-financial-news.p.rapidapi.com'
-      }
-    };
+    // const options = {
+    //   method: 'GET',
+    //   url: 'https://bloomberg-market-and-financial-news.p.rapidapi.com/market/auto-complete',
+    //   params: {query: '<REQUIRED>'},
+    //   headers: {
+    //     'X-RapidAPI-Key': 'b27ef0319dmshd9f173a6bfc4399p1e5a8djsn089ae46aa96f',
+    //     'X-RapidAPI-Host': 'bloomberg-market-and-financial-news.p.rapidapi.com'
+    //   }
+    // };
     
-    axios.request(options).then(function (response) {
-        console.log(response.data);
-    }).catch(function (error) {
-        console.error(error);
-    });
+    // axios.request(options).then(function (response) {
+    //     console.log(response.data);
+    // }).catch(function (error) {
+    //     console.error(error);
+    // });
 
     return(
         <>
