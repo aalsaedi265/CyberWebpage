0c3eda7a7c81332695ddf51385e8990eb2839cd6
aalsaedi265 <aalsaedi265@gmail.com>
2022-11-06 11:32:51 -0600

    paths to different pages established

--- code/src/App.js
+++ code/src/App.js
@@ -6,7 +6,7 @@ import NighCityNews from './modules/news_feed/NightCityNews';
 import TopHunter from './modules/Top_hunters';
 import TopHacker from './modules/Top_hackers';
 import PastConversation from './modules/Past_convo'
-import Edgerunner from './modules/Edgerunner'
+import MyProfile from './modules/MyProfile'
 
 import './App.css';
 
@@ -27,11 +27,12 @@ function App() {
         </div>
           <MenuSelect/>
           <Routes>
-            <Route exact path = '/' element={ <Edgerunner/>}/>
-            <Route exact path= 'nighcity-news' element={<NighCityNews/>}/>
-            <Route exact path ='most-dependable-people' element={<TopHunter/> } />
-            <Route exact path = 'best-hackers' element={<TopHacker/>} />
-            <Route exact path = 'conversation-log' element = {<PastConversation/>}/>
+            <Route exact path = '/'  />
+            <Route exact path= 'nighcity_news' element={<NighCityNews/>}/>
+            <Route exact path = "personal_page" element={<MyProfile/>}/>
+            <Route exact path ='most_dependable_people' element={<TopHunter/> } />
+            <Route exact path = 'best_hackers' element={<TopHacker/>} />
+            <Route exact path = 'conversation_log' element = {<PastConversation/>}/>
           </Routes>
       </div>
     </body>
--- code/src/modules/MenuSelect.js
+++ code/src/modules/MenuSelect.js
@@ -10,11 +10,12 @@ function menuSelect()
          <div>
             <ul>
                     
-                <Link to= 'nighcity-news' style={{textDecoration:'none'}}> <li>Nigh City News </li></Link> 
-                <Link to= '/' style={{textDecoration:'none'}}>  <li>Your Page</li> </Link>
-                <Link to='most-dependable-people' style={{textDecoration:'none'}}>   <li>Best Around </li></Link>
-                <Link to='conversation-log' style={{textDecoration:'none'}} ><li>Past Convos</li> </Link>
-                <Link to='best-hackers' style={{textDecoration:'none'}}>  <li>NetRunners</li> </Link>
+                <Link to= 'nighcity_news' style={{textDecoration:'none'}}> <li>Tech News</li></Link> 
+                <Link to= 'personal_page' style={{textDecoration:'none'}}>  <li>Your Page</li> </Link>
+                <Link to='most_dependable_people' style={{textDecoration:'none'}}>   <li>Best Around </li></Link>
+                <Link to='conversation_log' style={{textDecoration:'none'}} ><li>Past Convos</li> </Link>
+                <Link to='best_hackers' style={{textDecoration:'none'}}>  <li>NetRunners</li> </Link>
+                <Link to= "/" style={{textDecoration:'none'}}><li>Genesis</li></Link>
                 
             </ul>
         </div>
